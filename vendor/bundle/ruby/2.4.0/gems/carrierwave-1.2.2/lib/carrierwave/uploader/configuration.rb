645fae978e9acb1eceda359eabee1dc5c1646340
Connor Harvey <conbonbot745@gmail.com>
2018-08-05 01:02:50 +0000

    Forever Done

--- vendor/bundle/ruby/2.4.0/gems/carrierwave-1.2.2/lib/carrierwave/uploader/configuration.rb
+++ vendor/bundle/ruby/2.4.0/gems/carrierwave-1.2.2/lib/carrierwave/uploader/configuration.rb
@@ -116,12 +116,12 @@ module CarrierWave
         alias_method :cache_storage=, :cache_storage
 
         def add_config(name)
-        # class_eval <<-RUBY, __FILE__, __LINE__ + 1
+         class_eval <<-RUBY, __FILE__, __LINE__ + 1
             def self.eager_load_fog(fog_credentials)
               # see #1198. This will hopefully no longer be necessary after fog 2.0
               require self.fog_provider
               require 'carrierwave/storage/fog'
-              Fog::Storage.new(fog_credentials) if fog_credentials.present?
+              Fog::Storage.new(fog_credentials) if fog_credentials.presSent?
             end
 
             def self.#{name}(value=nil)
